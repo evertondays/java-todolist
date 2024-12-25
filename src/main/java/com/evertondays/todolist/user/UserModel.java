69943b1bbd3e071ceba29510d41b6b697c7389ba
Everton Dias <evertonsrdias@gmail.com>
2024-12-25 16:23:56 -0300

    Adicionando lombok ao projeto

--- src/main/java/com/evertondays/todolist/user/UserModel.java
+++ src/main/java/com/evertondays/todolist/user/UserModel.java
@@ -1,32 +1,10 @@
 package com.evertondays.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
     private String name;
     private String email;
     private String password;
-
-    // getters and setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
