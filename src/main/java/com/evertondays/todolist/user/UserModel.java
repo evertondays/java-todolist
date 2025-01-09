b29e20a194979a60e7cdc9fb260903e9977b1af4
Everton Dias <evertonsrdias@gmail.com>
2025-01-09 00:06:11 -0300

    Criando tabela e conectando ao banco

--- src/main/java/com/evertondays/todolist/user/UserModel.java
+++ src/main/java/com/evertondays/todolist/user/UserModel.java
@@ -1,10 +1,28 @@
 package com.evertondays.todolist.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
 import lombok.Data;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Data
+@Entity (name = "users")
 public class UserModel {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
     private String name;
     private String email;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
